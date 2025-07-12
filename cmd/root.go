d9fe4e9639a6e5c39d7460347899d5a8ea65aa99
canpok1 <6isstrong@gmail.com>
2025-07-12 13:25:38 +0000

    コマンドの説明を追加

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -16,13 +13,13 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ai-feed",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "An AI-powered CLI RSS reader that summarizes articles and posts comments to various platforms.",
+	Long: `ai-feed is a command-line interface (CLI) RSS reader designed to
+streamline your news consumption. It fetches and caches articles
+from your subscribed feeds, allowing you to browse them efficiently.
+What sets ai-feed apart is its ability to leverage AI to generate
+insightful comments on articles and even post them to platforms
+like Slack and Misskey.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -40,15 +37,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ai-feed.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
