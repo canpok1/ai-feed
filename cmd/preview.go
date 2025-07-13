5858f1e8ca4b29abbfef31b3a163149259309374
canpok1 <6isstrong@gmail.com>
2025-07-13 01:18:47 +0000

    feat: previewコマンドに--limitオプションを追加 (#6)
    - previewコマンドに--limitオプションを追加し、表示する記事の最大件数を指定できるようにしました。
    - 複数のURLが指定された場合、全てのリソースから取得した記事を合算し、指定された件数で制限します。
    - --limitオプションが0以下の場合は制限を適用せず、全て表示します。

--- cmd/preview.go
+++ cmd/preview.go
@@ -20,25 +20,40 @@ anything to your local cache.`,
 		if err != nil {
 			return err
 		}
+
+		limit, err := cmd.Flags().GetInt("limit")
+		if err != nil {
+			return err
+		}
+
 		loc, err := time.LoadLocation("Asia/Tokyo")
 		if err != nil {
 			return err
 		}
+
+		var allArticles []*internal.Article
 		for _, url := range urls {
 			articles, err := internal.FetchFeed(url)
 			if err != nil {
 				fmt.Fprintf(cmd.ErrOrStderr(), "Error fetching feed from %s: %v\n", url, err)
 				continue
 			}
-			for _, article := range articles {
-				fmt.Printf("Title: %s\n", article.Title)
-				fmt.Printf("Link: %s\n", article.Link)
-                if article.Published != nil {
-                        fmt.Printf("Published: %s\n", article.Published.In(loc).Format("2006-01-02 15:04:05 JST"))
-                }
-                fmt.Printf("Content: %s\n", article.Content)
-                fmt.Println("---")
+			allArticles = append(allArticles, articles...)
+		}
+
+		// Apply limit
+		if limit > 0 && len(allArticles) > limit {
+			allArticles = allArticles[:limit]
+		}
+
+		for _, article := range allArticles {
+			fmt.Printf("Title: %s\n", article.Title)
+			fmt.Printf("Link: %s\n", article.Link)
+			if article.Published != nil {
+				fmt.Printf("Published: %s\n", article.Published.In(loc).Format("2006-01-02 15:04:05 JST"))
 			}
+			fmt.Printf("Content: %s\n", article.Content)
+			fmt.Println("---")
 		}
 		return nil
 	},
@@ -47,4 +62,5 @@ anything to your local cache.`,
 func init() {
 	rootCmd.AddCommand(previewCmd)
 	previewCmd.Flags().StringSliceP("url", "u", []string{}, "URL of the feed to preview")
+	previewCmd.Flags().IntP("limit", "l", 0, "Maximum number of articles to display")
 }
