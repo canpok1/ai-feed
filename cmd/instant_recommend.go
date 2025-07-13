7ea6d8ca9c241ddbd8d65e48429f8b8d8e7af11f
canpok1 <6isstrong@gmail.com>
2025-07-13 12:26:08 +0000

    feat: instant-recommendコマンドに--sourceオプションを追加
    fixed #18

--- cmd/instant_recommend.go
+++ cmd/instant_recommend.go
@@ -51,5 +51,6 @@ func init() {
 	rootCmd.AddCommand(instantRecommendCmd)
 
 	instantRecommendCmd.Flags().StringP("url", "u", "", "URL of the feed to recommend from")
+	instantRecommendCmd.Flags().StringP("source", "s", "", "Path to a file containing a list of URLs")
 	instantRecommendCmd.MarkFlagRequired("url")
 }
