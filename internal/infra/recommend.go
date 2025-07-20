5dcd9b2b121a54bbf8162597b277d4043ac15e73
canpok1 <6isstrong@gmail.com>
2025-07-20 14:01:32 +0000

    CommentGeneratorの生成処理周りを実装

---
+++ internal/infra/recommend.go
@@ -0,0 +1,37 @@
+package infra
+
+import (
+	"fmt"
+
+	"github.com/canpok1/ai-feed/internal/domain"
+	"github.com/canpok1/ai-feed/internal/domain/entity"
+)
+
+var factoryMap = map[string]func(entity.AIModelConfig, entity.PromptConfig) domain.CommentGenerator{
+	"gemini-2.5-flash": newGeminiCommentGenerator,
+	"gemini-2.5-pro":   newGeminiCommentGenerator,
+}
+
+func MakeCommentGenerator(model entity.AIModelConfig, prompt entity.PromptConfig) (domain.CommentGenerator, error) {
+	if f, ok := factoryMap[model.Type]; ok {
+		return f(model, prompt), nil
+	}
+	return nil, fmt.Errorf("unsupported model type: %s", model.Type)
+}
+
+type geminiCommentGenerator struct {
+	model  entity.AIModelConfig
+	prompt entity.PromptConfig
+}
+
+func newGeminiCommentGenerator(model entity.AIModelConfig, prompt entity.PromptConfig) domain.CommentGenerator {
+	return &geminiCommentGenerator{
+		model:  model,
+		prompt: prompt,
+	}
+}
+
+func (g *geminiCommentGenerator) Generate(article entity.Article) (string, error) {
+	// TODO 実装
+	return "生成したコメント", nil
+}
