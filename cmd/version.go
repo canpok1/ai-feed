59f206e04f5fb70573c83509acab46017676d7a4
canpok1 <6isstrong@gmail.com>
2025-08-16 13:48:45 +0000

    feat: versionサブコマンドの実装を追加

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// version はビルド時にldflags で埋め込まれるバージョン情報
+var version = "dev"
+
+// makeVersionCmd はversionコマンドを生成する
+func makeVersionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "バージョン情報を表示",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println(version)
+			return nil
+		},
+	}
+	return cmd
+}
