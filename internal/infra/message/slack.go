3a3fcd17248d15b321c176f28639fa217eb77f21
canpok1 <6isstrong@gmail.com>
2025-09-06 05:27:38 +0000

    feat(message): Slack送信時に表示名とアイコンを指定できるようにする

--- internal/infra/message/slack.go
+++ internal/infra/message/slack.go
@@ -16,9 +16,9 @@ type SlackTemplateData struct {
 }
 
 type SlackSender struct {
-	client    *slack.Client
-	channelID string
-	tmpl      *template.Template
+	client *slack.Client
+	config *entity.SlackAPIConfig
+	tmpl   *template.Template
 }
 
 func NewSlackSender(config *entity.SlackAPIConfig) domain.MessageSender {
@@ -30,13 +30,13 @@ func NewSlackSender(config *entity.SlackAPIConfig) domain.MessageSender {
 	tmpl := template.Must(template.New("slack_message").Parse(*config.MessageTemplate))
 
 	return &SlackSender{
-		client:    slack.New(config.APIToken),
-		channelID: config.Channel,
-		tmpl:      tmpl,
+		client: slack.New(config.APIToken),
+		config: config,
+		tmpl:   tmpl,
 	}
 }
 
-func (v *SlackSender) SendRecommend(recommend *entity.Recommend, fixedMessage string) error {
+func (s *SlackSender) SendRecommend(recommend *entity.Recommend, fixedMessage string) error {
 	// テンプレートデータを作成
 	templateData := &SlackTemplateData{
 		Article:      &recommend.Article,
@@ -46,17 +46,30 @@ func (v *SlackSender) SendRecommend(recommend *entity.Recommend, fixedMessage st
 
 	// パース済みテンプレートを直接実行
 	var buf bytes.Buffer
-	if err := v.tmpl.Execute(&buf, templateData); err != nil {
+	if err := s.tmpl.Execute(&buf, templateData); err != nil {
 		return err
 	}
 
-	return v.postMessage(buf.String())
+	return s.postMessage(buf.String())
 }
 
-func (v *SlackSender) postMessage(msg string) error {
-	_, _, err := v.client.PostMessage(
-		v.channelID,
+func (s *SlackSender) postMessage(msg string) error {
+	options := []slack.MsgOption{
 		slack.MsgOptionText(msg, false),
+	}
+	if s.config.Username != "" {
+		options = append(options, slack.MsgOptionUsername(s.config.Username))
+	}
+	if s.config.IconURL != "" {
+		options = append(options, slack.MsgOptionIconURL(s.config.IconURL))
+	}
+	if s.config.IconEmoji != "" {
+		options = append(options, slack.MsgOptionIconEmoji(s.config.IconEmoji))
+	}
+
+	_, _, err := s.client.PostMessage(
+		s.config.Channel,
+		options...,
 	)
 	return err
 }
