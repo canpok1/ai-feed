62914bcc5d3ae5438b1eb5f1d948b41904627d50
canpok1 <6isstrong@gmail.com>
2025-12-07 03:50:43 +0000

    claude codeの設定をホストと共有しないよう修正

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+CLAUDE_DIR=/home/vscode/.claude
+
+ln -s ${SCRIPT_DIR}/.claude ${CLAUDE_DIR}
+
 npm install -g @google/gemini-cli
 
 cd "$(dirname "$0")/.."
